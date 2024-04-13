874e91c02fe55a251e74f700471a9ed9efbc8b61
Radz Santillan <116485181+radz22@users.noreply.github.com>
2024-04-13 23:18:26 +0800

    chanmges

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ dotenv.config();
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
-  );
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
